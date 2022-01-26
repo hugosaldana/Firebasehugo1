00b3eb74c4a1fc947f34db54750f51d3818b09a4
hugo <hugo.saldana@institutvidreres.cat>
2022-01-26 19:56:03 +0100

    final de ñññ

--- app/src/main/java/com/example/firebase/clients/ClientsFragment.kt
+++ app/src/main/java/com/example/firebase/clients/ClientsFragment.kt
@@ -1,6 +1,8 @@
 package com.example.firebase.clients
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.firebase.R
 import com.example.firebase.databinding.FragmentClientsBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ClientsFragment : Fragment() {
@@ -24,15 +28,28 @@ class ClientsFragment : Fragment() {
         binding.clientsenrrere.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_clientsFragment_to_homeFragment)
         }
+        val db = Firebase.firestore
 
 
-
-        binding.clientsregisterbutton.setOnClickListener { view : View ->
+        binding.CrearButton.setOnClickListener { view : View ->
             val nom = binding.clientsnom.text.toString()
             val email = binding.clientsemail.text.toString()
             val contr = binding.clientscontrasenya.text.toString()
 
+            val user = hashMapOf(
+                "Email" to email,
+                "Nom" to nom,
+                "Password" to contr
+            )
 
+            db.collection("Users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
         }
 
         // Inflate the layout for this fragment
--- app/src/main/java/com/example/firebase/login/LoginFragment.kt
+++ app/src/main/java/com/example/firebase/login/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.firebase.login
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +11,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.firebase.R
 import com.example.firebase.databinding.FragmentLoginBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+
 import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
-    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +26,28 @@ class LoginFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
             R.layout.fragment_login,container,false)
 
-        auth = Firebase.auth
-
+        val db = Firebase.firestore
 
 
         binding.loginbutton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val contr = binding.editTextTextPassword.text.toString()
+
+            val user = hashMapOf(
+                "Email" to email,
+                "Password" to contr
+            )
+
+            db.collection("Users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
 
         }
 
